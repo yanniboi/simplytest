df492b6c9dd6351e84563a77733decdc95c22d97
Patrick Drotleff <patrick.d@ventral.org>
2015-06-02 01:03:15 +0200

    QR Code has been deprecated by short URLs was removed. Also QR codes havent been cool anymore for a while now.

--- scripts/snippet.php
+++ scripts/snippet.php
@@ -88,21 +88,12 @@ function _simplytest_snippet_infobar($variables) {
           text-decoration: none;
         }
 
-        #simplytest-snippet-qr-code {
-          position: relative;
-          top: 2px;
-          margin-left: 10px;
-          vertical-align: top !important;
-        }
-
-        #simplytest-snippet-close,
-        #simplytest-snippet-qr-code {
+        #simplytest-snippet-close {
           cursor: pointer;
         }
 
         @media only screen and (max-width: 18.125em) {
-          #simplytest-snippet-backlink,
-          #simplytest-snippet-qr-code {
+          #simplytest-snippet-backlink {
             display: none;
           }
         }
@@ -112,7 +103,6 @@ function _simplytest_snippet_infobar($variables) {
       <div id="simplytest-snippet-container">
         <span id="simplytest-snippet-infobar">
           <span id="simplytest-snippet-countdown-time"></span>
-          <img id="simplytest-snippet-qr-code" src="data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAMAAAAoLQ9TAAAAM1BMVEUAAAD29vb+/v76+vr4+Pj09PT5+fnz8/Pw8PD4+Pj19fX29vbz8/P39/fy8vLu7u75+flIu1cCAAAAAXRSTlMAQObYZgAAAAFiS0dEAIgFHUgAAAAJcEhZcwAACxMAAAsTAQCanBgAAAAHdElNRQfdBQ4NAQBbiP8AAAAAaUlEQVQY02WNCQ6AIAwER0XAouD/X2vLZaJTGja7TQs/1luBC/tXNXxtNwQctZzrAsta3gWXgfddDJZlypT0pUIO7NIsEYSYCTvvXIGgkcnTUEMnEJsIrSNjR65V3h2x99yxFYWNz5XJAxK4A86fTNrwAAAAAElFTkSuQmCC" title="Generate QR code" alt="QR Code Image" />
           <a href="<? print 'http://simplytest.me/project/' . urlencode($project) . '/' . urlencode($version); ?>" id="simplytest-snippet-backlink" title="Back to simplytest.me"><?php print $save_project; ?> <?php print $save_version; ?></a>
           <span id="simplytest-snippet-close" title="Hide bar">&#x2718;</span>
         </span>
@@ -170,7 +160,6 @@ function _simplytest_snippet_infobar($variables) {
 
           var barContainer = get('simplytest-snippet-container');
           var barElement = get('simplytest-snippet-infobar');
-          var barQrCode = get('simplytest-snippet-qr-code');
           var barClose = get('simplytest-snippet-close');
           var barOpen = get('simplytest-snippet-open');
           var toggle = false;
@@ -187,19 +176,6 @@ function _simplytest_snippet_infobar($variables) {
           };
           barClose.onclick = toggleSimplytestInfobar;
           barOpen.onclick = toggleSimplytestInfobar;
-
-          // QR code functionality.
-          var displayQrCode = function (e) {
-            if (e) { e.preventDefault(); }
-            var currentURL = window.location.hostname + window.location.pathname;
-            var width = 200;
-            var height = 200;
-            var wx = (screen.width - width) >> 1;
-            var wy = (screen.height - height) >> 1;
-            var url = 'http://chart.googleapis.com/chart?cht=qr&chl=http://' + currentURL + '&chs=200x200';
-            window.open(url, '', 'top=' + wy + ',left=' + wx + ',width=' + width + ',height=' + height);
-          };
-          barQrCode.onclick = displayQrCode;
 
           // Preset form fields (admin username / password, mysql credentials).
           if (get('edit-site-name') !== null) {
