c50cbfc7f2a51427776d41a73a48501c68d0abc8
yanniboi <yanniboi@1837556.no-reply.drupal.org>
2017-05-15 14:46:18 +0000

    By yanniboi: Added very simple subtheme of bartik.

--- modules/simplytest_submission/src/Form/SubmissionForm.php
+++ modules/simplytest_submission/src/Form/SubmissionForm.php
@@ -2,23 +2,12 @@
 
 namespace Drupal\simplytest_submission\Form;
 
-use Drupal\Core\Form\FormStateInterface;
-
 /**
  * Form controller for Submission edit forms.
  *
  * @ingroup simplytest_submission
  */
 class SubmissionForm extends SubmissionFormBase {
-
-  /**
-   * {@inheritdoc}
-   */
-  public function alterForm(array &$form, FormStateInterface $form_state) {
-    parent::alterForm($form, $form_state);
-    // Add submission theme library.
-    $form['#attached']['library'][] = 'simplytest_submission/submission';
-  }
 
   /**
    * {@inheritdoc}
